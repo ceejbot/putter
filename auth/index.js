633049caab211c06648e92ade04a6170a536c8dc
C J Silverio <ceej@npmjs.com>
2015-02-15 18:15:31 -0800

    Sketching more things out.

---
+++ auth/index.js
@@ -0,0 +1,10 @@
+var
+    _       = require('lodash'),
+    assert  = require('assert'),
+    restify = require('restify')
+    ;
+
+var AuthServer = module.exports = function APIServer(options)
+{
+    // TODO insert code here
+};
