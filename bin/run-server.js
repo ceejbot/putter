1cc44e4d92247d64424ca3f41d74e318c4cf45a9
C J Silverio <ceej@npmjs.com>
2015-04-01 11:38:42 -0700

    A start at jscs rules for ceej-style.

--- bin/run-server.js
+++ bin/run-server.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 var
-    assert = require('assert'),
-    bole   = require('bole'),
-    path   = require('path'),
-    argv   = require('yargs')
-        .usage('run-server --node <nodename> <servertype>')
-        .demand(1)
-        .alias('n', 'node')
-        .describe('n', 'a name for this node')
-        .help('h')
-        .alias('h', 'help')
-        .argv
-    ;
+	assert = require('assert'),
+	bole   = require('bole'),
+	path   = require('path'),
+	argv   = require('yargs')
+		.usage('run-server --node <nodename> <servertype>')
+		.demand(1)
+		.alias('n', 'node')
+		.describe('n', 'a name for this node')
+		.help('h')
+		.alias('h', 'help')
+		.argv
+	;
 
 require('toml-require').install();
 var type = argv._[0];
@@ -22,7 +22,7 @@ var config = require(path.join(__dirname, '..', 'config', segment + '.toml'));
 if (argv.n) config.name = argv.n;
 
 if (process.env.PORT)
-    config.port = process.env.PORT;
+	config.port = process.env.PORT;
 
 var logger = bole('wrapper');
 var outputs = [];
@@ -39,5 +39,5 @@ bole.output(outputs);
 var server = new Server(config);
 server.listen(config.port, config.host, function()
 {
-    logger.info('data api service started at ' + config.host + ':' + config.port);
+	logger.info('data api service started at ' + config.host + ':' + config.port);
 });
