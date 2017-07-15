b31a598b7446317db2a03c51e0ef58b8d25fb587
C J Silverio <ceejceej@gmail.com>
2017-07-15 12:26:24 -0700

    A script to seed the tag auto-complete redis.

---
+++ bin/seed-completers.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+require('dotenv').config();
+
+const
+	completer = require('prefix-completer'),
+	dbconn    = require('../lib/db-conn'),
+	redis     = require('redis'),
+	Tag       = require('../lib/models/tag')
+	;
+
+dbconn();
+
+const tcompl = completer.create( { key: 'tags', db: 1 } );
+
+Tag.all().then(tags =>
+{
+	const texts = tags.map(t => t.tag);
+	tcompl.add(texts, (err, added) =>
+	{
+		if (err) throw(err);
+		console.log(`I guess we added ${added.length} tags to the completer`);
+		process.exit(0);
+	});
+
+}).catch(err =>
+{
+	console.log(err.message);
+	process.exit(1);
+});
+
